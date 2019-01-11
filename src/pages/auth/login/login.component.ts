7743f984119f3ecc7c24105b7b7ee44569eaed45
Jesus Macedo <macedo.rjpablo@gmail.com>
2019-01-11 16:32:32 -0600

    Implemented login inside proper auth module.

--- src/pages/auth/login/login.component.ts
+++ src/pages/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { auth } from 'firebase';
 
 @Component({
     selector: 'compras-login',
@@ -6,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
-    constructor() {}
+    constructor(private firebase: AngularFireAuth, private router: Router) {}
+
+    // * Angular Lifecycle
 
     ngOnInit() {}
+
+    // * User Interaction
+
+    /**
+     * Display the auth page in order to access the Google user account.
+     */
+    didPressGoogle(): void {
+        this.firebase.auth.signInWithPopup(new auth.GoogleAuthProvider()).then((credentials: auth.UserCredential) => {
+            console.log('credentials', credentials);
+            this.router.navigate(['compras/dashboard']);
+        });
+    }
 }
