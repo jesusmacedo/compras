0cdd31a7fe16c4c13f2b065cea966ea2e75fba72
Jesus Macedo <macedo.rjpablo@gmail.com>
2019-01-11 16:56:07 -0600

    Created and added HeaderComponent.

--- src/components/shared/shared.module.ts
+++ src/components/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { HeaderComponent } from './header/header.component';
 
 /**
  * `module` for declaring, importing and exporting features that are used across the whole app.
  */
 @NgModule({
-    declarations: [],
+    declarations: [HeaderComponent],
     imports: [CommonModule, TranslateModule],
-    exports: [TranslateModule]
+    exports: [TranslateModule, HeaderComponent]
 })
 export class SharedModule {}
--- src/pages/lists/lists.module.ts
+++ src/pages/lists/lists.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/components/shared/shared.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ListsRoutingModule } from './lists-routing.module';
 
 @NgModule({
     declarations: [DashboardComponent],
-    imports: [CommonModule, ListsRoutingModule]
+    imports: [CommonModule, ListsRoutingModule, SharedModule]
 })
 export class ListsModule {}
